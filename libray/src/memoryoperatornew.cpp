03346f205660eaa9e4d2c43a312bc66aaee81a6f
alterhz <550360139@qq.com>
2015-09-21 20:27:37 +0800

    修改linux下的内存泄露检测代码

--- libray/src/memoryoperatornew.cpp
+++ libray/src/memoryoperatornew.cpp
@@ -1,7 +1,7 @@
 #include "memoryoperatornew.h"
 #include "memorystatistics.h"
 
-void * operator new(unsigned int size, const char *file, int line)
+void * operator new(std::size_t size, const char *file, int line)
 {
 	// 下面两种方法可以达到同样的效果，但下面一种比较好  
 	// 因为用下面一种可以保持原有的申请方式一样  
@@ -16,24 +16,19 @@ void * operator new(unsigned int size, const char *file, int line)
 	return p;
 }
 
-void operator delete(void *p)
+void * operator new[](std::size_t size, const char *file, int line)
 {
-	free(p);
+	// 下面两种方法可以达到同样的效果，但下面一种比较好  
+	// 因为用下面一种可以保持原有的申请方式一样  
+	//void * p = malloc(size);
+	void *p = operator new(size);
 
 	if (CMemoryStatistics::m_bOpen)
 	{
-		CMemoryStatistics::getMe().Delete(p);
+		CMemoryStatistics::getMe().New(p, size, file, line);
 	}
-}
 
-void operator delete [](void *p)
-{
-	free(p);
-
-	if (CMemoryStatistics::m_bOpen)
-	{
-		CMemoryStatistics::getMe().Delete(p);
-	}
+	return p;
 }
 
 void operator delete(void *p, const char *file, int line)
@@ -56,3 +51,44 @@ void operator delete [](void *p, const char *file, int line)
 	}
 }
 
+#ifdef WIN32
+void operator delete(void * p) 
+{
+	free(p);
+
+	if (CMemoryStatistics::m_bOpen)
+	{
+		CMemoryStatistics::getMe().Delete(p);
+	}
+}
+
+void operator delete [](void * p) 
+{
+	free(p);
+
+	if (CMemoryStatistics::m_bOpen)
+	{
+		CMemoryStatistics::getMe().Delete(p);
+	}
+}
+#else
+void operator delete(void * p) noexcept
+{
+	free(p);
+
+	if (CMemoryStatistics::m_bOpen)
+	{
+		CMemoryStatistics::getMe().Delete(p);
+	}
+}
+
+void operator delete [](void * p) noexcept
+{
+	free(p);
+
+	if (CMemoryStatistics::m_bOpen)
+	{
+		CMemoryStatistics::getMe().Delete(p);
+	}
+}
+#endif // WIN32
--- public/include/memoryoperatornew.h
+++ public/include/memoryoperatornew.h
@@ -6,14 +6,28 @@
 #ifndef _MEMORYOPERATORNEW_H_
 #define _MEMORYOPERATORNEW_H_
 
-void * operator new(unsigned int size, const char *file, int line);
+#include <new>
 
-void operator delete(void *p);
+void * operator new(std::size_t size, const char *file, int line);
 
-void operator delete [](void *p);
+void * operator new[](std::size_t size, const char *file, int line);
 
 void operator delete(void *p, const char *file, int line);
 
 void operator delete [](void *p, const char *file, int line);
+
+#ifdef WIN32
+
+void operator delete( void *ptr );
+
+void operator delete[]( void *ptr );
+
+#else
+
+void operator delete( void *ptr ) noexcept;
+
+void operator delete[]( void *ptr ) noexcept;
+
+#endif // WIN32
 
 #endif
